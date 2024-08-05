509e20ddf988ba3d028b4ef3629ceb3a87f649da
Rob Adams <134746213+illinoisrobert@users.noreply.github.com>
2024-08-05 15:02:35 -0500

    Robadams cicd (#1)
    Add automatic generation and pushing of docker images.
    
    If a branch is being pushed, then docker image pcesapps-dev is updated with the latest and timestamp tags.
    
    If a tag is being pushed, then docker image pcesapps-beta is updated with the latest, timestamp, and git tag tags.
    
    If the docker-build.sh script is invoked by hand, with no arguments, then pcseapps-test is updated with a timestamp tag.

--- beta/docker-build.sh
+++ beta/docker-build.sh
@@ -1,9 +1,46 @@
-#!/bin/sh -ex
+#!/bin/bash -e
 # Copyright 2024 The Board of Trustees of the University of Illinois
 
-TAG=ghcr.io/iti/pcesapps-beta:v0.1.1
-TAG2=ghcr.io/iti/pcesapps-beta:latest
-cp ../go.mod ../go.sum .
-docker build --no-cache -t $TAG -t $TAG2 .
-docker push $TAG
-docker push $TAG2
+# Usage: docker-build.sh [suffix [version ...]]
+# Builds a docker image
+# and pushes it with the tags
+#   ghcr.io/iti/pocesapps-$suffix:$version1
+#   ghcr.io/iti/pocesapps-$suffix:$version2
+#   ...
+# The default value of "$suffix" is test.
+# Additionally, versions of $(date) and "latest"
+# are always created
+#
+# To test your code from the command line:
+#   ./docker-build.sh
+#   ./docker-run.sh
+# That creates ghcr.io.iti/pcesapps-test:latest
+#
+# Whenever you git push a branch, the ci/cd runs:
+#   ./docker-build.sh dev
+# This creates ghcr.io/iti/pcesappos-dev:latest
+#
+# Whever your git push a tag, the ci/cd runs:
+#  ./docker-build.shy beta $tag
+# This creates ghcr.io/iti/pcesapps-beta:$tag
+
+#pfx="ghcr.io/illinoisadams/doc-test"
+pfx="ghcr.io/iti/pcesapps"
+
+now=$(date -u +%F-%H-%M-%S)
+if "$1" = ""
+then
+    image="$pfx-test"
+    latest=
+else
+    image="$pfx-$1"
+    shift
+    latest=( "latest" "$@" )
+fi
+
+docker build --no-cache -t "$image:$now" .
+docker push "$image:$now"
+for v in "${latest[@]}" ; do
+	docker tag "$image:$now" "$image:$v"
+	docker push "$image:$v"
+done
